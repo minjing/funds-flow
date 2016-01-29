1f925f2a446c8312f2273bcea7426a1748fced71
xquan <xiaoming.quan@tekcomms.com>
2016-01-29 17:26:05 +0800

    Replace ListView with Recyclerview for tag list

--- app/src/main/java/ministudio/fundsflow/tag/TagFragment.java
+++ app/src/main/java/ministudio/fundsflow/tag/TagFragment.java
@@ -1,18 +1,18 @@
 package ministudio.fundsflow.tag;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.BaseAdapter;
-import android.widget.ListView;
 import android.widget.TextView;
 
+import ministudio.fundsflow.DividerItemDecoration;
 import ministudio.fundsflow.R;
 import ministudio.fundsflow.SQLitePersistence;
 
@@ -26,8 +26,8 @@ public class TagFragment extends Fragment implements SwipeRefreshLayout.OnRefres
     private int _tagTypeId;
 
     private SwipeRefreshLayout _tagListLayout;
-    private ListView _tagListView;
-    private TagAdapter _tagAdapter;
+    private RecyclerView _tagListView;
+    private TagListAdapter _tagAdapter;
 
     void setActivity(TagActivity activity) {
         this._activity = activity;
@@ -50,20 +50,21 @@ public class TagFragment extends Fragment implements SwipeRefreshLayout.OnRefres
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_tag, container, false);
 
-        this._tagListView = (ListView) rootView.findViewById(R.id.tag_list);
         this._tagListLayout = (SwipeRefreshLayout) rootView.findViewById(R.id.tag_layout);
         this._tagListLayout.setOnRefreshListener(this);
 
+        this._tagListView = (RecyclerView) rootView.findViewById(R.id.tag_list);
+        this._tagListView.setHasFixedSize(true);
+        this._tagListView.setItemAnimator(new DefaultItemAnimator());
+        this._tagListView.addItemDecoration(new DividerItemDecoration(this._activity, DividerItemDecoration.VERTICAL_LIST));
+
         Tag[] tags = Tag.findByType(this._persistence, this._tagTypeId);
-        this._tagAdapter = new TagAdapter(this._activity, tags);
+        this._tagAdapter = new TagListAdapter(tags);
+
+        LinearLayoutManager layoutMgr = new LinearLayoutManager(this._activity);
+        layoutMgr.setOrientation(LinearLayoutManager.VERTICAL);
+        this._tagListView.setLayoutManager(layoutMgr);
         this._tagListView.setAdapter(this._tagAdapter);
-        this._tagListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int pos, long id) {
-                Tag tag = (Tag) TagFragment.this._tagAdapter.getItem(pos);
-                new TagEditor().createUI(TagFragment.this._activity, TagFragment.this._tagTypeId, tag);
-            }
-        });
 
         return rootView;
     }
@@ -92,63 +93,61 @@ public class TagFragment extends Fragment implements SwipeRefreshLayout.OnRefres
         this._tagAdapter.notifyDataSetChanged();
     }
 
-    private static final class TagAdapter extends BaseAdapter {
+    private final class TagListAdapter extends RecyclerView.Adapter<TagListAdapter.TagListViewHolder> {
 
         private Tag[] _tags;
-        private final Context _ctx;
 
-        private TagAdapter(Context context, Tag[] tags) {
-            this._ctx = context;
+        private TagListAdapter(Tag[] tags) {
             this._tags = tags;
+        }
+
+        @Override
+        public TagListViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_tag, parent, false);
+            TagListViewHolder viewHolder = new TagListViewHolder(view);
+            return viewHolder;
+        }
+
+        @Override
+        public void onBindViewHolder(TagListViewHolder holder, int position) {
+            holder._pos = position;
+            Tag tag = this._tags[position];
+            holder._labTagName.setText(tag.getName());
+            TagCategory tagCat = this._tags[position].getCategory();
+            if (tagCat != null) {
+                holder._labTagCatName.setVisibility(View.VISIBLE);
+                holder._labTagCatName.setText(tag.getCategory().getName());
+            } else {
+                holder._labTagCatName.setVisibility(View.INVISIBLE);
+                holder._labTagCatName.setText(null);
+            }
+        }
+
+        @Override
+        public int getItemCount() {
+            return this._tags.length;
         }
 
         void update(Tag[] tags) {
             this._tags = tags;
         }
 
-        @Override
-        public int getCount() {
-            return this._tags.length;
-        }
+        public final class TagListViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+            private TextView _labTagName;
+            private TextView _labTagCatName;
+            private int _pos;
 
-        @Override
-        public Object getItem(int i) {
-            return this._tags[i];
-        }
-
-        @Override
-        public long getItemId(int i) {
-            return this._tags[i].getId();
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            Holder holder;
-            if (convertView == null) {
-                LayoutInflater inflater = LayoutInflater.from(this._ctx);
-                convertView = inflater.inflate(R.layout.list_tag, null);
-                holder = new Holder();
-                holder._labTagName = (TextView) convertView.findViewById(R.id.tag_name);
-                holder._labTagCat = (TextView) convertView.findViewById(R.id.tag_cat_name);
-                convertView.setTag(holder);
-            } else {
-                holder = (Holder) convertView.getTag();
+            private TagListViewHolder(View rootView) {
+                super(rootView);
+                this._labTagName = (TextView) rootView.findViewById(R.id.lab_tag_name);
+                this._labTagCatName = (TextView) rootView.findViewById(R.id.lab_tag_cat_name);
+                rootView.setOnClickListener(this);
             }
-            holder._labTagName.setText(this._tags[position].getName());
-            TagCategory tagCat = this._tags[position].getCategory();
-            if (tagCat != null) {
-                holder._labTagCat.setVisibility(View.VISIBLE);
-                holder._labTagCat.setText(tagCat.getName());
-            } else {
-                holder._labTagCat.setVisibility(View.INVISIBLE);
-                holder._labTagCat.setText(null);
-            }
-            return convertView;
-        }
-    }
 
-    private static final class Holder {
-        private TextView _labTagName;
-        private TextView _labTagCat;
+            @Override
+            public void onClick(View v) {
+                new TagEditor().createUI(TagFragment.this._activity, TagFragment.this._tagTypeId, TagListAdapter.this._tags[this._pos]);
+            }
+        }
     }
 }
